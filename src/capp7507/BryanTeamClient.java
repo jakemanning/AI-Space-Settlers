d24e47a1a636c21c58b3322e9723bb1639d0bfe7
Bryan Capps <bryanacapps@gmail.com>
2018-02-05 10:30:03 -0600

    Getting intelligent with it

--- src/capp7507/BryanTeamClient.java
+++ src/capp7507/BryanTeamClient.java
@@ -2,82 +2,225 @@ package capp7507;
 
 import spacesettlers.actions.*;
 import spacesettlers.clients.TeamClient;
-import spacesettlers.graphics.CircleGraphics;
 import spacesettlers.graphics.SpacewarGraphics;
-import spacesettlers.objects.AbstractActionableObject;
-import spacesettlers.objects.AbstractObject;
-import spacesettlers.objects.Ship;
+import spacesettlers.objects.*;
 import spacesettlers.objects.powerups.SpaceSettlersPowerupEnum;
 import spacesettlers.objects.resources.ResourcePile;
-import spacesettlers.objects.weapons.AbstractWeapon;
 import spacesettlers.simulator.Toroidal2DPhysics;
 import spacesettlers.utilities.Position;
+import spacesettlers.utilities.Vector2D;
 
 import java.util.*;
+
 /**
- * A team of random agents
+ * A team of alright agents
  *
- * The agents pick a random location in space and aim for it.  They shoot somewhat randomly also.
- * @author amy
+ * The agents move towards the closest enemy ship and shoots when it gets close.
+ * If it's low on energy then it goes to the nearest beacon or friendly base.
+ * Kinda avoids obstacles, but not very well.
+ * @author Bryan Capps
  *
  */
 public class BryanTeamClient extends TeamClient {
-	HashSet<SpacewarGraphics> graphics;
-	boolean fired = false;
-
-	public static int RANDOM_MOVE_RADIUS = 200;
-	public static double SHOOT_PROBABILITY = 0.1;
+	private HashSet<SpacewarGraphics> graphics;
+	private AbstractObject target;
+	private boolean avoiding = false;
 
 	@Override
 	public void initialize(Toroidal2DPhysics space) {
-		graphics = new HashSet<SpacewarGraphics>();
+		graphics = new HashSet<>();
 	}
 
 	@Override
 	public void shutDown(Toroidal2DPhysics space) {
-		// TODO Auto-generated method stub
-
 	}
-
 
 	@Override
 	public Map<UUID, AbstractAction> getMovementStart(Toroidal2DPhysics space,
 													  Set<AbstractActionableObject> actionableObjects) {
-		HashMap<UUID, AbstractAction> randomActions = new HashMap<UUID, AbstractAction>();
+		HashMap<UUID, AbstractAction> actions = new HashMap<>();
 
-		for (AbstractObject actionable :  actionableObjects) {
+		for (AbstractActionableObject actionable :  actionableObjects) {
+			Position currentPosition = actionable.getPosition();
+
 			if (actionable instanceof Ship) {
 				Ship ship = (Ship) actionable;
-				AbstractAction current = ship.getCurrentAction();
-
-				// if we finished, make a new spot in space to aim for
-				if (current == null || current.isMovementFinished(space)) {
-					Position currentPosition = ship.getPosition();
-					Position newGoal = space.getRandomFreeLocationInRegion(random, Ship.SHIP_RADIUS, (int) currentPosition.getX(),
-							(int) currentPosition.getY(), RANDOM_MOVE_RADIUS);
-					MoveAction newAction = null;
-					newAction = new MoveAction(space, currentPosition, newGoal);
-					//System.out.println("Ship is at " + currentPosition + " and goal is " + newGoal);
-					SpacewarGraphics graphic = new CircleGraphics(1, getTeamColor(), newGoal);
-					graphics.add(graphic);
-					//Vector2D shortVec = space.findShortestDistanceVector(currentPosition, newGoal);
-					//LineShadow lineShadow = new LineShadow(currentPosition, newGoal, shortVec);
-					//newShadows.add(lineShadow);
-					randomActions.put(ship.getId(), newAction);
+				Collection<AbstractObject> targets = new HashSet<>();
+				if (goingToCrash(space, ship)) {
+					AbstractAction action = avoidCrashAction(space, currentPosition);
+					avoiding = true;
+					actions.put(ship.getId(), action);
+					return actions;
+				} else if (shipNeedsEnergy(ship)) {
+					targets.addAll(getEnergySources(space, getTeamName()));
+				} else if (ship.getResources().getTotal() > 1000) {
+					System.out.println(ship.getResources());
+					targets.addAll(getTeamBases(space, getTeamName()));
 				} else {
-					randomActions.put(ship.getId(), ship.getCurrentAction());
+					targets.addAll(getEnemyShips(space, getTeamName()));
+					Set<Asteroid> asteroids = getMineableAsteroids(space);
+					targets.addAll(asteroids);
 				}
-
-			} else {
-				// it is a base and random doesn't do anything to bases
-				randomActions.put(actionable.getId(), new DoNothingAction());
+				avoiding = false;
+				AbstractObject target = closest(space, currentPosition, targets);
+				MoveAction action = getMoveAction(space, currentPosition, target);
+				this.target = target;
+				actions.put(ship.getId(), action);
+			} else if (actionable instanceof Base) {
+				Base base = (Base) actionable;
+				this.target = base;
+				actions.put(base.getId(), new DoNothingAction());
 			}
-
-
 		}
 
-		return randomActions;
+		return actions;
+	}
 
+	private boolean goingToCrash(Toroidal2DPhysics space, Ship ship) {
+		Position shipPosition = ship.getPosition();
+		Vector2D shipVelocity = shipPosition.getTranslationalVelocity();
+		Set<AbstractActionableObject> ships = getOtherShips(space, ship.getId());
+		Set<Asteroid> asteroids = getNonMineableAsteroids(space);
+		Set<Base> bases = space.getBases();
+		Set<AbstractObject> obstacles = new HashSet<>();
+		obstacles.addAll(ships);
+		obstacles.addAll(asteroids);
+		obstacles.addAll(bases);
+		for (AbstractObject obstacle : obstacles) {
+			if (obstacle == target) continue;
+			Vector2D obstacleVector = space.findShortestDistanceVector(shipPosition, obstacle.getPosition());
+			double angleDifference = Math.abs(obstacleVector.getAngle() - shipVelocity.getAngle());
+			boolean onCourse = angleDifference < Math.PI / 8;
+			if (onCourse) {
+				double distance = obstacleVector.getMagnitude();
+				double stoppingDistance = ship.getPosition().getTranslationalVelocity().getMagnitude();
+				if (distance < stoppingDistance * 3) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	private AbstractAction avoidCrashAction(Toroidal2DPhysics space, Position currentPosition) {
+		if (avoiding) {
+			return new DoNothingAction();
+		}
+
+		Vector2D currentVelocity = currentPosition.getTranslationalVelocity();
+		double angleDiff = 2 * Math.PI / 3;
+		double newAngle = currentVelocity.getAngle() + angleDiff;
+		if (target != null) {
+			Vector2D targetVector = space.findShortestDistanceVector(currentPosition, target.getPosition());
+			double targetAngle = targetVector.getAngle();
+			double currentAngle = currentVelocity.getAngle();
+			if (targetAngle - currentAngle < 0) {
+				newAngle = newAngle - 2*angleDiff;
+			}
+		}
+		Vector2D newVel = Vector2D.fromAngle(newAngle, currentVelocity.getMagnitude() * 6);
+		Position newTarget = currentPosition.deepCopy();
+		newTarget.setTranslationalVelocity(newVel);
+		return new MoveAction(space, currentPosition, newTarget);
+	}
+
+	private Set<Base> getTeamBases(Toroidal2DPhysics space, String teamName) {
+		Set<Base> results = new HashSet<>();
+		for (Base base : space.getBases()) {
+			if (Objects.equals(base.getTeamName(), teamName)) {
+				results.add(base);
+			}
+		}
+		return results;
+	}
+
+	private Set<Asteroid> getNonMineableAsteroids(Toroidal2DPhysics space) {
+		Set<Asteroid> results = new HashSet<>();
+		for (Asteroid asteroid : space.getAsteroids()) {
+			if (!asteroid.isMineable()) {
+				results.add(asteroid);
+			}
+		}
+		return results;
+	}
+
+	private Set<Asteroid> getMineableAsteroids(Toroidal2DPhysics space) {
+		Set<Asteroid> results = new HashSet<>();
+		for (Asteroid asteroid : space.getAsteroids()) {
+			if (asteroid.isMineable()) {
+				results.add(asteroid);
+			}
+		}
+		return results;
+	}
+
+	private MoveAction getMoveAction(Toroidal2DPhysics space, Position currentPosition, AbstractObject target) {
+		Position targetPosition = target.getPosition();
+		double targetSpeed = targetPosition.getTranslationalVelocity().getMagnitude();
+		Vector2D distanceVector = space.findShortestDistanceVector(currentPosition, targetPosition);
+		double magnitude = (distanceVector.getMagnitude() > 30) ? 20 : targetSpeed;
+		double angle = distanceVector.getAngle();
+		return new MoveAction(space, currentPosition, targetPosition, Vector2D.fromAngle(angle, magnitude));
+	}
+
+	private Set<AbstractObject> getEnergySources(Toroidal2DPhysics space, String teamName) {
+		Set<AbstractObject> energySources = new HashSet<>();
+		energySources.addAll(space.getBeacons());
+		Set<Base> ourBases = new HashSet<>();
+		for (Base base : space.getBases()) {
+			if (Objects.equals(base.getTeamName(), teamName) && base.getHealingEnergy() > 1000) {
+				ourBases.add(base);
+			}
+		}
+		energySources.addAll(ourBases);
+		return energySources;
+	}
+
+	private boolean shipNeedsEnergy(Ship ship) {
+		return ship.getEnergy() < ship.getMaxEnergy() * 0.2;
+	}
+
+	private <T extends AbstractObject> T closest(Toroidal2DPhysics space, Position currentPosition,
+												 Collection<T> objects) {
+		T closest = null;
+		double minimum = Double.MAX_VALUE;
+		for (T object : objects) {
+			double distance = space.findShortestDistance(currentPosition, object.getPosition());
+			if (object instanceof Asteroid) {
+				Asteroid asteroid = (Asteroid) object;
+				distance = distance / asteroid.getMass();
+			}
+			if (distance < minimum) {
+				minimum = distance;
+				closest = object;
+			}
+		}
+		return closest;
+	}
+
+	private Set<AbstractActionableObject> getEnemyShips(Toroidal2DPhysics space, String teamName) {
+		Set<AbstractActionableObject> enemies = new HashSet<>();
+		for (Ship ship : space.getShips()) {
+			if (!Objects.equals(ship.getTeamName(), teamName)) {
+				enemies.add(ship);
+			}
+		}
+		for (Base base : space.getBases()) {
+			if (!base.isHomeBase() && !Objects.equals(base.getTeamName(), teamName)) {
+				enemies.add(base);
+			}
+		}
+		return enemies;
+	}
+
+	private Set<AbstractActionableObject> getOtherShips(Toroidal2DPhysics space, UUID id) {
+		Set<AbstractActionableObject> ships = new HashSet<>();
+		for (Ship ship : space.getShips()) {
+			if (!Objects.equals(ship.getId(), id)) {
+				ships.add(ship);
+			}
+		}
+		return ships;
 	}
 
 
@@ -87,22 +230,82 @@ public class BryanTeamClient extends TeamClient {
 
 	@Override
 	public Set<SpacewarGraphics> getGraphics() {
-		HashSet<SpacewarGraphics> newGraphics = new HashSet<SpacewarGraphics>(graphics);
+		HashSet<SpacewarGraphics> newGraphics = new HashSet<>(graphics);
 		graphics.clear();
 		return newGraphics;
 	}
 
-
-	@Override
 	/**
-	 * Random never purchases
+	 * If there is enough resourcesAvailable, buy a base.  Place it by finding a ship that is sufficiently
+	 * far away from the existing bases
+	 * Yeah boiiiiiiiii
 	 */
 	public Map<UUID, PurchaseTypes> getTeamPurchases(Toroidal2DPhysics space,
 													 Set<AbstractActionableObject> actionableObjects,
 													 ResourcePile resourcesAvailable,
 													 PurchaseCosts purchaseCosts) {
-		return new HashMap<UUID,PurchaseTypes>();
 
+		HashMap<UUID, PurchaseTypes> purchases = new HashMap<UUID, PurchaseTypes>();
+		double BASE_BUYING_DISTANCE = 200;
+		boolean bought_base = false;
+
+		if (purchaseCosts.canAfford(PurchaseTypes.BASE, resourcesAvailable)) {
+			for (AbstractActionableObject actionableObject : actionableObjects) {
+				if (actionableObject instanceof Ship) {
+					Ship ship = (Ship) actionableObject;
+					Set<Base> bases = space.getBases();
+
+					// how far away is this ship to a base of my team?
+					double maxDistance = Double.MIN_VALUE;
+					for (Base base : bases) {
+						if (base.getTeamName().equalsIgnoreCase(getTeamName())) {
+							double distance = space.findShortestDistance(ship.getPosition(), base.getPosition());
+							if (distance > maxDistance) {
+								maxDistance = distance;
+							}
+						}
+					}
+
+					if (maxDistance > BASE_BUYING_DISTANCE) {
+						purchases.put(ship.getId(), PurchaseTypes.BASE);
+						bought_base = true;
+						//System.out.println("Buying a base!!");
+						break;
+					}
+				}
+			}
+		}
+
+		// see if you can buy EMPs
+		if (purchaseCosts.canAfford(PurchaseTypes.POWERUP_EMP_LAUNCHER, resourcesAvailable)) {
+			for (AbstractActionableObject actionableObject : actionableObjects) {
+				if (actionableObject instanceof Ship) {
+					Ship ship = (Ship) actionableObject;
+
+					if (!ship.isValidPowerup(PurchaseTypes.POWERUP_EMP_LAUNCHER.getPowerupMap())) {
+						purchases.put(ship.getId(), PurchaseTypes.POWERUP_EMP_LAUNCHER);
+					}
+				}
+			}
+		}
+
+
+		// can I buy a ship?
+		if (purchaseCosts.canAfford(PurchaseTypes.SHIP, resourcesAvailable) && !bought_base) {
+			for (AbstractActionableObject actionableObject : actionableObjects) {
+				if (actionableObject instanceof Base) {
+					Base base = (Base) actionableObject;
+
+					purchases.put(base.getId(), PurchaseTypes.SHIP);
+					break;
+				}
+
+			}
+
+		}
+
+
+		return purchases;
 	}
 
 	/**
@@ -111,22 +314,39 @@ public class BryanTeamClient extends TeamClient {
 	public Map<UUID, SpaceSettlersPowerupEnum> getPowerups(Toroidal2DPhysics space,
 														   Set<AbstractActionableObject> actionableObjects) {
 
-		HashMap<UUID, SpaceSettlersPowerupEnum> powerupMap = new HashMap<UUID, SpaceSettlersPowerupEnum>();
+		HashMap<UUID, SpaceSettlersPowerupEnum> powerupMap = new HashMap<>();
 
 		for (AbstractObject actionable :  actionableObjects) {
 			if (actionable instanceof Ship) {
 				Ship ship = (Ship) actionable;
-				if (random.nextDouble() < SHOOT_PROBABILITY) {
-					AbstractWeapon newBullet = ship.getNewWeapon(SpaceSettlersPowerupEnum.FIRE_MISSILE);
-					if (newBullet != null) {
-						powerupMap.put(ship.getId(), SpaceSettlersPowerupEnum.FIRE_MISSILE);
-						//System.out.println("Firing!");
-					}
+				Set<AbstractActionableObject> enemyShips = getEnemyShips(space, getTeamName());
+				AbstractActionableObject closestEnemyShip = closest(space, ship.getPosition(), enemyShips);
+				if (inPositionToShoot(space, ship.getPosition(), closestEnemyShip) && !shipNeedsEnergy(ship)) {
+					shoot(powerupMap, ship);
 				}
 			}
 		}
 		return powerupMap;
 	}
 
+	private void shoot(HashMap<UUID, SpaceSettlersPowerupEnum> powerupMap, Ship ship) {
+		if (random.nextDouble() < 0.35) {
+			powerupMap.put(ship.getId(), SpaceSettlersPowerupEnum.FIRE_MISSILE);
+		}
+	}
+
+	private boolean inPositionToShoot(Toroidal2DPhysics space, Position currentPosition,
+									  AbstractActionableObject target) {
+		Position targetPosition = target.getPosition();
+		boolean close = space.findShortestDistance(currentPosition, targetPosition) < 100;
+		if (!close) {
+			return false;
+		}
+		Vector2D targetVector = space.findShortestDistanceVector(currentPosition, targetPosition);
+		double targetAngle = targetVector.getAngle();
+		double currentAngle = currentPosition.getOrientation();
+		double angleDifference = Math.abs(targetAngle - currentAngle);
+		return angleDifference < Math.PI / 12;
+	}
 
 }
---
+++ src/capp7507/JakeTeamClient.java
@@ -0,0 +1,291 @@
+package capp7507;
+
+import spacesettlers.actions.*;
+import spacesettlers.clients.TeamClient;
+import spacesettlers.graphics.SpacewarGraphics;
+import spacesettlers.objects.*;
+import spacesettlers.objects.powerups.SpaceSettlersPowerupEnum;
+import spacesettlers.objects.resources.ResourcePile;
+import spacesettlers.simulator.Toroidal2DPhysics;
+import spacesettlers.utilities.Position;
+import spacesettlers.utilities.Vector2D;
+
+import java.util.*;
+
+/**
+ * A team of alright agents
+ *
+ * The agents move towards things with the "best value" and shoots when it gets close to enemy ships.
+ * If it's low on energy then it goes to the nearest beacon or friendly base.
+ * Kinda avoids obstacles, but not very well.
+ * @author Bryan Capps
+ *
+ */
+public class JakeTeamClient extends TeamClient {
+	private HashSet<SpacewarGraphics> graphics;
+	private AbstractObject target;
+
+	@Override
+	public void initialize(Toroidal2DPhysics space) {
+		graphics = new HashSet<>();
+	}
+
+	@Override
+	public void shutDown(Toroidal2DPhysics space) {
+	}
+
+	@Override
+	public Map<UUID, AbstractAction> getMovementStart(Toroidal2DPhysics space,
+													  Set<AbstractActionableObject> actionableObjects) {
+		HashMap<UUID, AbstractAction> actions = new HashMap<>();
+
+		for (AbstractActionableObject actionable :  actionableObjects) {
+			Position currentPosition = actionable.getPosition();
+
+			if (actionable instanceof Ship) {
+				Ship ship = (Ship) actionable;
+				AbstractObject target = bestValue(space, ship, space.getAllObjects());
+				Position targetPosition = target.getPosition();
+				Vector2D targetVelocity = targetPosition.getTranslationalVelocity();
+				if (!(target instanceof Ship)) {
+					Vector2D toTarget = space.findShortestDistanceVector(currentPosition, targetPosition);
+					double angle = toTarget.getAngle();
+					targetVelocity = Vector2D.fromAngle(angle, 30);
+				}
+				MoveAction action = new MoveAction(space, currentPosition, targetPosition, targetVelocity);
+				actions.put(ship.getId(), action);
+			} else if (actionable instanceof Base) {
+				Base base = (Base) actionable;
+				this.target = base;
+				actions.put(base.getId(), new DoNothingAction());
+			}
+		}
+
+		return actions;
+	}
+
+	private boolean shipNeedsEnergy(Ship ship) {
+		return ship.getEnergy() < ship.getMaxEnergy() * 0.2;
+	}
+
+	private AbstractObject bestValue(Toroidal2DPhysics space, Ship ship,
+												   Collection<AbstractObject> objects) {
+		AbstractObject best = null;
+		double maximum = Double.MIN_VALUE;
+		for (AbstractObject object : objects) {
+			double rawDistance = space.findShortestDistance(ship.getPosition(), object.getPosition());
+			double scaledDistance = Math.log1p(rawDistance);
+			double value = 0;
+			if (object instanceof Asteroid) {
+				Asteroid asteroid = (Asteroid) object;
+				if (asteroid.isMineable()) {
+					value = Math.log1p(asteroid.getMass());
+				}
+			} else if (object instanceof AbstractActionableObject) {
+				AbstractActionableObject actionableObject = (AbstractActionableObject) object;
+				if (isEnemyTarget(actionableObject)) {
+					value = Math.log1p(5 * energyValue(actionableObject));
+				} else if (isOurBase(actionableObject)) {
+					value = Math.log1p(5 * energyValue(ship) + cargoValue(ship));
+				}
+			} else if (object instanceof Beacon) {
+				value = Math.log1p(5 * energyValue(ship));
+			}
+			double score = value / scaledDistance;
+			if (score > maximum) {
+				maximum = score;
+				best = object;
+			}
+		}
+		return best;
+	}
+
+	private boolean isEnemyTarget(AbstractActionableObject actionableObject) {
+		if (actionableObject instanceof Base) {
+			Base base = (Base) actionableObject;
+			if (base.isHomeBase()) {
+				return false;
+			}
+		}
+		return !actionableObject.getTeamName().equals(getTeamName());
+	}
+
+	private boolean isOurBase(AbstractActionableObject actionableObject) {
+		return actionableObject instanceof Base && actionableObject.getTeamName().equals(getTeamName());
+	}
+
+	private double energyValue(AbstractActionableObject ship) {
+		double percentEnergy = ship.getEnergy() / ship.getMaxEnergy();
+		if (percentEnergy > 0.8) {
+			return 10;
+		} else if (percentEnergy < 0.3) {
+			return 50000;
+		} else {
+			return percentEnergy * 1000;
+		}
+	}
+
+	private double cargoValue(Ship ship) {
+		final int total = ship.getResources().getTotal();
+		return total;
+	}
+
+	private Set<AbstractObject> getEnemyShips(Toroidal2DPhysics space, String teamName) {
+		Set<AbstractObject> enemies = new HashSet<>();
+		for (Ship ship : space.getShips()) {
+			if (!Objects.equals(ship.getTeamName(), teamName)) {
+				enemies.add(ship);
+			}
+		}
+		for (Base base : space.getBases()) {
+			if (!base.isHomeBase() && !Objects.equals(base.getTeamName(), teamName)) {
+				enemies.add(base);
+			}
+		}
+		return enemies;
+	}
+
+
+	@Override
+	public void getMovementEnd(Toroidal2DPhysics space, Set<AbstractActionableObject> actionableObjects) {
+	}
+
+	@Override
+	public Set<SpacewarGraphics> getGraphics() {
+		HashSet<SpacewarGraphics> newGraphics = new HashSet<>(graphics);
+		graphics.clear();
+		return newGraphics;
+	}
+
+	/**
+	 * If there is enough resourcesAvailable, buy a base.  Place it by finding a ship that is sufficiently
+	 * far away from the existing bases
+	 */
+	public Map<UUID, PurchaseTypes> getTeamPurchases(Toroidal2DPhysics space,
+													 Set<AbstractActionableObject> actionableObjects,
+													 ResourcePile resourcesAvailable,
+													 PurchaseCosts purchaseCosts) {
+
+		HashMap<UUID, PurchaseTypes> purchases = new HashMap<UUID, PurchaseTypes>();
+		double BASE_BUYING_DISTANCE = 200;
+		boolean bought_base = false;
+
+		if (purchaseCosts.canAfford(PurchaseTypes.BASE, resourcesAvailable)) {
+			for (AbstractActionableObject actionableObject : actionableObjects) {
+				if (actionableObject instanceof Ship) {
+					Ship ship = (Ship) actionableObject;
+					Set<Base> bases = space.getBases();
+
+					// how far away is this ship to a base of my team?
+					double maxDistance = Double.MIN_VALUE;
+					for (Base base : bases) {
+						if (base.getTeamName().equalsIgnoreCase(getTeamName())) {
+							double distance = space.findShortestDistance(ship.getPosition(), base.getPosition());
+							if (distance > maxDistance) {
+								maxDistance = distance;
+							}
+						}
+					}
+
+					if (maxDistance > BASE_BUYING_DISTANCE) {
+						purchases.put(ship.getId(), PurchaseTypes.BASE);
+						bought_base = true;
+						//System.out.println("Buying a base!!");
+						break;
+					}
+				}
+			}
+		}
+
+		// see if you can buy EMPs
+		if (purchaseCosts.canAfford(PurchaseTypes.POWERUP_EMP_LAUNCHER, resourcesAvailable)) {
+			for (AbstractActionableObject actionableObject : actionableObjects) {
+				if (actionableObject instanceof Ship) {
+					Ship ship = (Ship) actionableObject;
+
+					if (!ship.isValidPowerup(PurchaseTypes.POWERUP_EMP_LAUNCHER.getPowerupMap())) {
+						purchases.put(ship.getId(), PurchaseTypes.POWERUP_EMP_LAUNCHER);
+					}
+				}
+			}
+		}
+
+
+		// can I buy a ship?
+		if (purchaseCosts.canAfford(PurchaseTypes.SHIP, resourcesAvailable) && !bought_base) {
+			for (AbstractActionableObject actionableObject : actionableObjects) {
+				if (actionableObject instanceof Base) {
+					Base base = (Base) actionableObject;
+
+					purchases.put(base.getId(), PurchaseTypes.SHIP);
+					break;
+				}
+
+			}
+
+		}
+
+
+		return purchases;
+	}
+
+	/**
+	 * This is the new way to shoot (and use any other power up once they exist)
+	 */
+	public Map<UUID, SpaceSettlersPowerupEnum> getPowerups(Toroidal2DPhysics space,
+														   Set<AbstractActionableObject> actionableObjects) {
+
+		HashMap<UUID, SpaceSettlersPowerupEnum> powerupMap = new HashMap<>();
+
+		for (AbstractObject actionable :  actionableObjects) {
+			if (actionable instanceof Ship) {
+				Ship ship = (Ship) actionable;
+				Collection<AbstractObject> enemyShips = getEnemyShips(space, getTeamName());
+				AbstractObject closestEnemyShip = closest(space, ship.getPosition(), enemyShips);
+				if (inPositionToShoot(space, ship.getPosition(), closestEnemyShip) && !shipNeedsEnergy(ship)) {
+					shoot(powerupMap, ship);
+				}
+			}
+		}
+		return powerupMap;
+	}
+
+	private void shoot(HashMap<UUID, SpaceSettlersPowerupEnum> powerupMap, Ship ship) {
+		if (random.nextDouble() < 0.3) {
+			powerupMap.put(ship.getId(), SpaceSettlersPowerupEnum.FIRE_MISSILE);
+		}
+	}
+
+	private <T extends AbstractObject> T closest(Toroidal2DPhysics space, Position currentPosition,
+												 Collection<T> objects) {
+		T closest = null;
+		double minimum = Double.MAX_VALUE;
+		for (T object : objects) {
+			double distance = space.findShortestDistance(currentPosition, object.getPosition());
+			if (object instanceof Asteroid) {
+				Asteroid asteroid = (Asteroid) object;
+				distance = distance / asteroid.getMass();
+			}
+			if (distance < minimum) {
+				minimum = distance;
+				closest = object;
+			}
+		}
+		return closest;
+	}
+
+	private boolean inPositionToShoot(Toroidal2DPhysics space, Position currentPosition,
+									  AbstractObject target) {
+		Position targetPosition = target.getPosition();
+		boolean close = space.findShortestDistance(currentPosition, targetPosition) < 100;
+		if (!close) {
+			return false;
+		}
+		Vector2D targetVector = space.findShortestDistanceVector(currentPosition, targetPosition);
+		double targetAngle = targetVector.getAngle();
+		double currentAngle = currentPosition.getOrientation();
+		double angleDifference = Math.abs(targetAngle - currentAngle);
+		return angleDifference < Math.PI / 12;
+	}
+
+}
